4da5a09fc91ac269589e823869ef3fb6d211bd0b
scepter914 <st14.828soccer@gmail.com>
2021-08-13 00:18:55 +0900

    add example

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
+extern crate rscam;
+
+use std::fs::File;
+use std::io::Write;
+
 fn main() {
-    println!("Hello, world!");
+    let mut camera = rscam::new("/dev/video0").unwrap();
+
+    camera
+        .start(&rscam::Config {
+            interval: (1, 330), // fps.
+            resolution: (640, 360),
+            format: b"MJPG",
+            ..Default::default()
+        })
+        .unwrap();
+    let mut count = 0;
+    loop {
+        count += 1;
+        let frame = camera.capture().unwrap();
+        if 500 <= count {
+            let mut file = File::create(&format!("data/frame_{:>03}.jpg", count - 500)).unwrap();
+            file.write_all(&frame[..]).unwrap();
+        }
+        if (500 + 500) < count {
+            break;
+        }
+    }
 }
