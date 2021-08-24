24740c161da47ceb83920b2a5e2911e812aa4d8e
scepter914 <st14.828soccer@gmail.com>
2021-08-25 02:10:38 +0900

    add Video struct
    Signed-off-by: scepter914 <st14.828soccer@gmail.com>

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
+extern crate ffmpeg_next as ffmpeg;
 extern crate image;
 extern crate log;
 extern crate rscam;
@@ -68,7 +69,68 @@ impl Camera {
     }
 }
 
-pub struct Video {}
+pub struct Video {
+    decoder: ffmpeg::decoder::Video,
+    ictx: ffmpeg::format::context::Input,
+    scaler: ffmpeg::software::scaling::Context,
+    width: u32,
+    height: u32,
+    video_stream_index: usize,
+}
+
+impl Video {
+    pub fn new(path: &std::path::Path) -> Video {
+        ffmpeg::init().unwrap();
+        let mut ictx_ = ffmpeg::format::input(&path).unwrap();
+        let input = ictx_.streams().best(ffmpeg::media::Type::Video).unwrap();
+        let video_stream_index = input.index();
+
+        let mut decoder_ = input.codec().decoder().video().unwrap();
+        let width_ = decoder_.width();
+        let height_ = decoder_.height();
+
+        let mut scaler_ = ffmpeg::software::scaling::context::Context::get(
+            decoder_.format(),
+            width_,
+            height_,
+            ffmpeg::format::Pixel::RGB24,
+            decoder_.width(),
+            decoder_.height(),
+            ffmpeg::software::scaling::flag::Flags::BILINEAR,
+        )
+        .unwrap();
+
+        Video {
+            decoder: decoder_,
+            ictx: ictx_,
+            scaler: scaler_,
+            width: width_,
+            height: height_,
+            video_stream_index,
+        }
+    }
+
+    pub fn get_frame(&mut self) -> Option<image::RgbImage> {
+        let (stream, packet) = self.ictx.packets().next().unwrap();
+        if stream.index() == self.video_stream_index {
+            self.decoder.send_packet(&packet);
+            let mut decoded = ffmpeg::util::frame::video::Video::empty();
+            if self.decoder.receive_frame(&mut decoded).is_ok() {
+                let mut rgb_frame = ffmpeg::util::frame::video::Video::empty();
+                self.scaler.run(&decoded, &mut rgb_frame);
+
+                let rgb_image =
+                    image::RgbImage::from_vec(self.width, self.height, rgb_frame.data(0).to_vec())
+                        .unwrap();
+                Some(rgb_image)
+            } else {
+                None
+            }
+        } else {
+            None
+        }
+    }
+}
 
 pub struct Picture {
     image: image::RgbImage,
