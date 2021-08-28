3636aa50fd74b68d877734709ab1c1cf64a8746f
scepter914 <st14.828soccer@gmail.com>
2021-08-29 01:54:34 +0900

    change str to std::path::Path
    Signed-off-by: scepter914 <st14.828soccer@gmail.com>

--- src/lib.rs
+++ src/lib.rs
@@ -4,34 +4,54 @@ extern crate log;
 extern crate rscam;
 
 use log::{debug, error, info, trace, warn};
+use std::path::PathBuf;
+
+enum SimpleImageInterfaceMode {
+    Camera,
+    Video,
+    Picture,
+}
 
 pub struct SimpleImageInterface {
-    mode: String,
+    mode: SimpleImageInterfaceMode,
     camera: Option<Camera>,
     picture: Option<Picture>,
+    video: Option<Video>,
 }
 
 impl SimpleImageInterface {
     pub fn new_camera(device_: &str, width_: u32, height_: u32, fps_: u32) -> SimpleImageInterface {
         SimpleImageInterface {
-            mode: "Camera".to_string(),
+            mode: SimpleImageInterfaceMode::Camera,
             camera: Some(Camera::new(device_, width_, height_, fps_)),
             picture: None,
+            video: None,
         }
     }
 
-    pub fn new_picture(image_path: &str) -> SimpleImageInterface {
+    pub fn new_picture(image_path: impl Into<std::path::PathBuf>) -> SimpleImageInterface {
         SimpleImageInterface {
-            mode: "Picture".to_string(),
+            mode: SimpleImageInterfaceMode::Picture,
             camera: None,
-            picture: Some(Picture::new(image_path)),
+            picture: Some(Picture::new(image_path.into())),
+            video: None,
+        }
+    }
+
+    pub fn new_video(video_path: impl Into<std::path::PathBuf>) -> SimpleImageInterface {
+        SimpleImageInterface {
+            mode: SimpleImageInterfaceMode::Video,
+            camera: None,
+            picture: None,
+            video: Some(Video::new(video_path.into())),
         }
     }
 
     pub fn get_frame(&mut self) -> Option<image::RgbImage> {
-        match &self.mode[..] {
-            "Camera" => self.camera.as_ref().unwrap().get_frame(),
-            "Picture" => self.picture.as_mut().unwrap().get_frame(),
+        match self.mode {
+            SimpleImageInterfaceMode::Camera => self.camera.as_ref().unwrap().get_frame(),
+            SimpleImageInterfaceMode::Picture => self.picture.as_mut().unwrap().get_frame(),
+            SimpleImageInterfaceMode::Video => self.video.as_mut().unwrap().get_frame(),
             _ => None,
         }
     }
@@ -79,27 +99,30 @@ pub struct Video {
 }
 
 impl Video {
-    pub fn new(path: &std::path::Path) -> Video {
+    pub fn new(path: impl Into<std::path::PathBuf>) -> Video {
         ffmpeg::init().unwrap();
-        let mut ictx_ = ffmpeg::format::input(&path).unwrap();
+        let path_into = path.into();
+
+        let ictx_ = ffmpeg::format::input(&path_into).unwrap();
         let input = ictx_.streams().best(ffmpeg::media::Type::Video).unwrap();
         let video_stream_index = input.index();
 
-        let mut decoder_ = input.codec().decoder().video().unwrap();
+        let decoder_ = input.codec().decoder().video().unwrap();
         let width_ = decoder_.width();
         let height_ = decoder_.height();
 
-        let mut scaler_ = ffmpeg::software::scaling::context::Context::get(
+        let scaler_ = ffmpeg::software::scaling::context::Context::get(
             decoder_.format(),
             width_,
             height_,
             ffmpeg::format::Pixel::RGB24,
-            decoder_.width(),
-            decoder_.height(),
+            width_,
+            height_,
             ffmpeg::software::scaling::flag::Flags::BILINEAR,
         )
         .unwrap();
 
+        info!("Video {:?}: {} * {}", &path_into, width_, height_);
         Video {
             decoder: decoder_,
             ictx: ictx_,
@@ -113,20 +136,22 @@ impl Video {
     pub fn get_frame(&mut self) -> Option<image::RgbImage> {
         let (stream, packet) = self.ictx.packets().next().unwrap();
         if stream.index() == self.video_stream_index {
-            self.decoder.send_packet(&packet);
+            self.decoder.send_packet(&packet).unwrap();
             let mut decoded = ffmpeg::util::frame::video::Video::empty();
             if self.decoder.receive_frame(&mut decoded).is_ok() {
                 let mut rgb_frame = ffmpeg::util::frame::video::Video::empty();
-                self.scaler.run(&decoded, &mut rgb_frame);
+                self.scaler.run(&decoded, &mut rgb_frame).unwrap();
 
                 let rgb_image =
                     image::RgbImage::from_vec(self.width, self.height, rgb_frame.data(0).to_vec())
                         .unwrap();
                 Some(rgb_image)
             } else {
+                println!("hoge");
                 None
             }
         } else {
+            println!("fuga");
             None
         }
     }
@@ -140,11 +165,12 @@ pub struct Picture {
 }
 
 impl Picture {
-    pub fn new(image_path: &str) -> Picture {
-        let image_ = image::open(image_path).unwrap().to_rgb8();
+    pub fn new(image_path: impl Into<std::path::PathBuf>) -> Picture {
+        let image_path_into = image_path.into();
+        let image_ = image::open(&image_path_into).unwrap().to_rgb8();
         let width_ = image_.width();
         let height_ = image_.height();
-        info!("Picture {}: {} * {}", image_path, width_, height_);
+        info!("Picture {:?}: {} * {}", image_path_into, width_, height_);
         Picture {
             image: image_,
             width: width_,
