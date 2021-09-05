d5b37d0a221768822f99d7bfef4dfd2b6236ce45
scepter914 <st14.828soccer@gmail.com>
2021-09-05 17:48:48 +0900

    add tests
    Signed-off-by: scepter914 <st14.828soccer@gmail.com>

--- tests/tests.rs
+++ tests/tests.rs
@@ -1,5 +1,6 @@
 use simple_image_interface::SimpleImageInterface;
 use std::env;
+
 #[test]
 fn is_able_to_get_all_frames_from_mp4() {
     let mut interface = SimpleImageInterface::new_video("./data/random_ball.mp4");
@@ -16,7 +17,7 @@ fn is_able_to_get_all_frames_from_mp4() {
 }
 
 #[test]
-fn is_valid_picture_from_mp4() {
+fn is_valid_frame_from_mp4() {
     let mut interface = SimpleImageInterface::new_video("./data/random_ball.mp4");
     let mut frame_index = 0;
     let mut input_image;
@@ -26,9 +27,50 @@ fn is_valid_picture_from_mp4() {
         if frame_index == 200 {
             break;
         }
+        if input_image.is_none() {
+            frame_index -= 1;
+            break;
+        }
         assert!(!input_image.is_none(), "invalid frame is reading");
     }
     let path = "./data/sample_200.png";
+    let answer_img = image::open(path).unwrap().to_rgb8();
+    assert_eq!(input_image.unwrap(), answer_img);
+}
+
+#[test]
+fn is_able_to_get_all_frames_from_picture() {
+    let mut interface = SimpleImageInterface::new_picture("./data/from_raw.png");
+    let mut frame_index = 0;
+    loop {
+        frame_index += 1;
+        let input_image = interface.get_frame();
+
+        if input_image.is_none() {
+            frame_index -= 1;
+            break;
+        }
+    }
+    assert_eq!(1, frame_index);
+}
+
+#[test]
+fn is_valid_frame_from_picture() {
+    let mut interface = SimpleImageInterface::new_picture("./data/from_raw.png");
+    let mut frame_index = 0;
+    let mut input_image;
+    loop {
+        frame_index += 1;
+        input_image = interface.get_frame();
+        if frame_index == 1 {
+            break;
+        }
+        if input_image.is_none() {
+            frame_index -= 1;
+            break;
+        }
+    }
+    let path = "./data/from_raw.png";
     let answer_img = image::open(path).unwrap().to_rgb8();
     assert_eq!(input_image.unwrap(), answer_img);
 }
