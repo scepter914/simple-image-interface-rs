7d366517cd260ff4b0d8514b8c282bed94a2f862
scepter914 <st14.828soccer@gmail.com>
2021-09-05 18:12:26 +0900

    add docs
    Signed-off-by: scepter914 <st14.828soccer@gmail.com>

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,81 @@
+//! # simple-image-interface-rs
+//!
+//! - This repository is simple image interface library for rust.
+//!   - If you use this library, you can change easily between images, videos, and camera input.
+//!   - It may be useful for debug like robotics vision area.
+//! - Support image interface
+//!   - Camera
+//!     - [x] Web Camera (v4l2)
+//!     - [ ] Realsense
+//!     - [ ] Basler Camera
+//!   - Video
+//!     - [x] mp4
+//!   - Image
+//!     - [x] png
+//!     - [x] jpeg
+//!
+//! ## Get started
+//!
+//! - install for rscam
+//!
+//! ```
+//! sudo apt install libv4l-dev
+//! ```
+//!
+//! - install for ffmpeg-next
+//!
+//! ```
+//! sudo apt install -y clang libavcodec-dev libavformat-dev libavutil-dev pkg-config
+//! ```
+//!
+//! - Cargo.toml
+//!
+//! ```
+//! "simple_image_interface" = "0.1.0"
+//! ```
+//!
+//! - Make interface
+//!   - In detail, [See example code](example/examples.rs)
+//!
+//! ```rust
+//!
+//!     if args.len() < 2 || &args[1] == "pic" {
+//!         interface = SimpleImageInterface::new_picture("./data/from_raw.png");
+//!     } else if &args[1] == "video" {
+//!         interface = SimpleImageInterface::new_video("./data/random_ball.mp4");
+//!     } else {
+//!         interface = SimpleImageInterface::new_camera("/dev/video0", 640, 360, 330);
+//!         // width, height, fps
+//!     }
+//!
+//!     let mut frame_index = 0;
+//!     loop {
+//!         frame_index += 1;
+//!         let input_image = interface.get_frame();
+//!         if input_image.is_none() {
+//!             break;
+//!         }
+//!         my_image_proc(&input_image.unwrap(), frame_index);
+//!     }
+//! ```
+//!
+//! ## Note
+//!
+//! - Not use trait object but lapper struct to improve execution speed
+//!
+//! ## Reference
+//!
+//! - <https://github.com/loyd/rscam> : Use for Camera input
+//! - <https://github.com/zmwangx/rust-ffmpeg> : Use for Video input
+//!
+//!
+
 extern crate ffmpeg_next as ffmpeg;
 extern crate image;
 extern crate log;
 extern crate rscam;
 
 use log::{debug, error, info, trace, warn};
-use std::path::PathBuf;
 
 enum SimpleImageInterfaceMode {
     Camera,
@@ -20,6 +91,7 @@ pub struct SimpleImageInterface {
 }
 
 impl SimpleImageInterface {
+    /// - Init interface from web camera input
     pub fn new_camera(device_: &str, width_: u32, height_: u32, fps_: u32) -> SimpleImageInterface {
         SimpleImageInterface {
             mode: SimpleImageInterfaceMode::Camera,
@@ -29,6 +101,7 @@ impl SimpleImageInterface {
         }
     }
 
+    /// - Init interface from picture (png, jpeg)
     pub fn new_picture(image_path: impl Into<std::path::PathBuf>) -> SimpleImageInterface {
         SimpleImageInterface {
             mode: SimpleImageInterfaceMode::Picture,
@@ -38,6 +111,7 @@ impl SimpleImageInterface {
         }
     }
 
+    /// - Init interface from video (mp4)
     pub fn new_video(video_path: impl Into<std::path::PathBuf>) -> SimpleImageInterface {
         SimpleImageInterface {
             mode: SimpleImageInterfaceMode::Video,
@@ -47,6 +121,8 @@ impl SimpleImageInterface {
         }
     }
 
+    /// - get frame from interface
+    /// - If interface do not get a image, return None
     pub fn get_frame(&mut self) -> Option<image::RgbImage> {
         match self.mode {
             SimpleImageInterfaceMode::Camera => self.camera.as_ref().unwrap().get_frame(),
@@ -81,6 +157,7 @@ impl Camera {
             height: height_,
         }
     }
+
     pub fn get_frame(&self) -> Option<image::RgbImage> {
         let frame: rscam::Frame = self.camera.capture().unwrap();
         let rgb_image =
